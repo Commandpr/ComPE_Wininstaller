47e05497a8c1a24476f886f38bc3a64e763ffb87
Commandpr <745752287@qq.com>
2024-07-16 19:05:01 +0800

    图标更改为相对路径

--- ComPE_Wininstaller/resource.h
+++ ComPE_Wininstaller/resource.h
@@ -2,9 +2,10 @@
 // Microsoft Visual C++ 生成的包含文件。
 // 供 ComPE_Wininstaller1.rc 使用
 //
-#define IDI_ICON1                       101
 #define IDD_DIALOG1                     125
 #define IDD_DIALOG2                     127
+#define IDI_ICON2                       133
+#define IDI_ICON1                       133
 #define IDC_COMBO1                      1005
 #define IDC_RADIO1                      1006
 #define IDC_EDIT1                       1007
@@ -21,7 +22,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_RESOURCE_VALUE        134
 #define _APS_NEXT_COMMAND_VALUE         40002
 #define _APS_NEXT_CONTROL_VALUE         1018
 #define _APS_NEXT_SYMED_VALUE           101
